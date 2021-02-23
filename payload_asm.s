5988beeae9ae1c9f91e3c2b1f4769271eb87af36
Jarrod Cameron <z5210220@ad.unsw.edu.au>
2021-02-23 11:00:15 +1100

    added a few more comments

--- payload_asm.s
+++ payload_asm.s
@@ -29,27 +29,35 @@ main:
         ; Move &checkpoint into `eax`
         mov eax, [esp]
 
+        ; push NULL
         xor ebx, ebx
         push ebx
 
+        ; push "vid.mkv"
         lea ebx, [eax + (argv2-$) - (.checkpoint-$)]
         push ebx
 
+        ; push "/usr/bin/mpv"
         lea ebx, [eax + (argv1-$) - (.checkpoint-$)]
         push ebx
 
+        ; push NULL
         xor ebx, ebx
         push ebx
 
+        ; push "DISPLAY=:0"
         lea ebx, [eax + (envp3-$) - (.checkpoint-$)]
         push ebx
 
+        ; push "XDG_RUNTIME_DIR=/run/user/1000"
         lea ebx, [eax + (envp2-$) - (.checkpoint-$)]
         push ebx
 
+        ; push "HOME=/home/jc"
         lea ebx, [eax + (envp1-$) - (.checkpoint-$)]
         push ebx
 
+        ; execve("/usr/bin/mpv", ..., ...)
         lea edx, [esp]
         lea ecx, [esp + 16]
         lea ebx, [eax + (argv1-$) - (.checkpoint-$)]
